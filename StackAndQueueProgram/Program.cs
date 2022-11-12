366dfd75e315394be1f85116bb9d9e50ee445e8d
rahul01r <rrdubeynibm@gmail.com>
2022-11-12 22:43:40 +0530

    [rahul] Add:UC1-CreateAStack

---
+++ StackAndQueueProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace StackAndQueueProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+          StackAndQueue stack = new StackAndQueue();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Select Programs\n1.Create Stack");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Display();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ StackAndQueueProgram/StackAndQueue.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace StackAndQueueProgram
+{
+    public class StackAndQueue
+    {
+        private Node top;
+        public StackAndQueue()
+        {
+            this.top = null;
+        }
+        public void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} pushed to stack", value);
+        }
+        public void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
